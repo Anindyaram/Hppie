0bff145b9824fdca6542846f43385bee9fcf3d01
Anindya Tripathi <64327113+Anindyaram@users.noreply.github.com>
2023-05-17 12:46:47 +0530

    Added authentication

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,21 +1,25 @@
-import React from "react";
-import { useContext, useState, useEffect } from "react";
+import React, { useContext, useState, useEffect } from "react";
+
 import { useHistory } from "react-router-dom";
+
 import { auth } from "../firebase";
 
-const AuthContext = React.createContext;
+const AuthContext = React.createContext();
 
-export const useAuth = () => useContext(AuthContext);
-export const AuthProvider = ({ children }) => {
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export function AuthProvider({ children }) {
   const [loading, setLoading] = useState(true);
-  const [user, setUser] = useState({});
+  const [user, setUser] = useState(null);
   const history = useHistory();
 
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
       setUser(user);
       setLoading(false);
-      history.push("/chats");
+      if (user) history.push("/chats");
     });
   }, [user, history]);
 
@@ -26,4 +30,4 @@ export const AuthProvider = ({ children }) => {
       {!loading && children}
     </AuthContext.Provider>
   );
-};
+}
